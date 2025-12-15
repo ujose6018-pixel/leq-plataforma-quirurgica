2d66570b2849ba074c0fb4d710ec763ac854400d
ujose6018-pixel <ujose6018@gmail.com>
2025-12-15 00:14:15 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Esta configuración es necesaria para que Vercel y tu entorno local entiendan el proyecto React.
+export default defineConfig({
+  plugins: [react()],
+});
